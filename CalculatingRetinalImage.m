c365a6c5eda1ec7a2f76fc224acef3edb190bd9c
rubybouh <ruby.bouhassira@pennmedicine.upenn.edu>
2024-09-20 15:41:25 -0400

    Add files via upload

--- CalculatingRetinalImage.m
+++ CalculatingRetinalImage.m
@@ -1,7 +1,7 @@
 clear all
 close all
 
-% Calculating the retinal image using Fourier transforms 
+% Calculating the retinal image using Fourier transforms
 nPixels = 101;
 
 % Choose parameters and range of x values
@@ -77,9 +77,10 @@ retinal_image = system_matrix * display_image_sine;
 eigenvalues = diag(eigenvalues_matrix);
 
 % Sort the eigenvalues in ascending order and get the sorting indices
-[sorted_eigenvalues, sorting_indices] = sort(eigenvalues);
+[~, sorting_indices] = sort(eigenvalues);
 
 % Reorder the eigenvectors according to the sorted eigenvalues
+sorted_eigenvalues = eigenvalues(sorting_indices);
 sorted_eigenvectors = eigenvectors(:, sorting_indices);
 
 % Flipping evectors and evalues so evalues are in descending order
@@ -119,6 +120,7 @@ hold off
 % Creating the E_ifft matrix
 matrix = diag(ones(nPixels, 1));
 E_ifft = zeros(nPixels, nPixels);
+E_ifft_reordered = zeros(nPixels, nPixels);
 
 for i = (1:nPixels)
     col = ifftshift(ifft(matrix(:,i)));
@@ -127,24 +129,68 @@ end
 
 % Checking that the columns of E_ifft (complex exponentials) correspond to
 % linear combinations of columns in E_M
-E_M = desc_eigenvectors;
+
+% This did not work, most likely due to numerical precision errors
+% quotient1 = zeros(nPixels, central_column_index);
+% quotient2 = zeros(nPixels, central_column_index);
+
+% frequency = i;
+% eCheck1 = E_M(:,1+2*frequency-1:1+2*frequency)*(E_M(:,1+2*frequency-1:1+2*frequency)\E_ifft(:,1+frequency));
+% % We should have eCheck1 = E_ifft(:,1+frequency)
+% eCheck2 = E_M(:,1+2*frequency-1:1+2*frequency)*(E_M(:,1+2*frequency-1:1+2*frequency)\E_ifft(:,nPixels-frequency+1));
+% % We should have eCheck2 = E_ifft(:,nPixels-frequency+1)
+% quotient1(:, i) = eCheck1 .\ E_ifft(:,1+frequency);
+% quotient2(:, i) = eCheck2 .\ E_ifft(:,nPixels-frequency+1);
 
 frequencies = 1:(nPixels - 1)/2; 
-quotient1 = zeros(nPixels, central_column_index);
-quotient2 = zeros(nPixels, central_column_index);
+E_M = desc_eigenvectors;
+list1 = [];
+list2 = [];
 
 for i = (1:length(frequencies))
     frequency = i;
-    eCheck1 = E_M(:,1+2*frequency-1:1+2*frequency)*(E_M(:,1+2*frequency-1:1+2*frequency)\E_ifft(:,1+frequency));
-    % We should have eCheck1 = E_ifft(:,1+frequency)
-    eCheck2 = E_M(:,1+2*frequency-1:1+2*frequency)*(E_M(:,1+2*frequency-1:1+2*frequency)\E_ifft(:,nPixels-frequency+1));
-    % We should have eCheck2 = E_ifft(:,nPixels-frequency+1)
-    quotient1(:, i) = eCheck1 .\ E_ifft(:,1+frequency);
-    quotient2(:, i) = eCheck2 .\ E_ifft(:,nPixels-frequency+1);
+    % Get the regression weights on the whole eigenvector matrix E_M.
+    % Only two should be very big for each column of the E_ifft matrix.
+    weights1 = E_M\E_ifft(:,1+frequency);
+    weights2 = E_M\E_ifft(:,nPixels-frequency+1);
+    % weights1 and weights2 should be the same
+
+    absWeights1 = abs(weights1);
+    absWeights2 = abs(weights2);
+
+    % Finding largest and second largest weights
+    max_weight1 = max(absWeights1); 
+    max_index_weight1 = find(absWeights1 == max_weight1);
+    % Removing the max value so that we can find the second largest value
+    absWeights1(max_index_weight1) = [];
+    second_max_weight1 = max(absWeights1);
+    second_max_index_weight1 = find(absWeights1 == second_max_weight1);
+    list1{end + 1} = {max(max_index_weight1); max(second_max_index_weight1)};
+    % Put max() to account for the weight being the same at two indices
+    % (choose the larger one)
+
+    max_weight2 = max(absWeights2); 
+    max_index_weight2 = find(absWeights2 == max_weight2);
+    % Removing the max value so that we can find the second largest value
+    absWeights2(max_index_weight2) = [];
+    second_max_weight2 = max(absWeights2);
+    second_max_index_weight2 = find(absWeights2 == second_max_weight2);
+    list2{end + 1} = {max(max_index_weight2); max(second_max_index_weight2)};
+    % Put max() to account for the weight being the same at two indices
+    % (choose the larger one)
+
+    % Then reorder the E_ifft matrix
+    % Want to move the E_ifft column to the index of the weight
+    % Using the weights1 list
+    E_ifft_reordered(:,1+frequency) = E_ifft(:, list2{frequency}{1});
+    E_ifft_reordered(:,nPixels-frequency+1) = E_ifft(:, (list2{frequency}{1} + 1));
 end
 
-% Example: frequency = 1
-% eCheck1 = E_M(:,2:3)*(E_M(:,2:3)\E_ifft(:,2));
+% absWeights1 = sort(abs(weights1),'descend');
+% absWeights2 = sort(abs(weights2),'descend');
+
+% Using list 2 to reorder the MATLAB ifft matrix - should correspond to
+% increasing spatial frequency with increasing index.
 
 D_direct = inv(E_ifft) * system_matrix * E_ifft;
 retinal_image_fft_2 = E_ifft * (D_direct * (inv(E_ifft) * display_image_sine));
@@ -168,3 +214,20 @@ ylabel('Intensity');
 title('Intensity of Retinal Image (Output)');
 legend('Matrix Mult.', 'Fourier Transforms')
 hold off
+
+% FREQUENCY PLOT %
+
+figure(4);
+
+fs = 1/0.1;
+% 0.1 seconds between each sample (sampling period), so the signal is sampled 10 times per
+% second
+N = length(display_image_sine);
+Freq = (0:N-1)*(fs/N);
+% Scaling the indices to frequency values
+
+Y = E_ifft_reordered * (pointspread)';
+% Fourier transform of the pointspread
+
+% plot(Freq(1:round(N/2)), Y(1:round(N/2)));
+plot(Freq, Y)
